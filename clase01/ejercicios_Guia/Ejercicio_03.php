10f79f8426c4e0bdf534953eaf642a0566f09b8b
Patricio Perez Cardenal <patricio.perezcardenal@gmail.com>
2023-03-27 21:53:05 -0300

    PRACTICAS PROGRA_3 - Ejercicio 03 [CLASE 01]

---
+++ clase01/ejercicios_Guia/Ejercicio_03.php
@@ -0,0 +1,10 @@
+<?php
+$x = -3;
+$y = 15;
+
+$resultado = $x + $y;
+
+echo "X = $x <br>" . //Las variables pueden ser colocadas dentro de las comillas ya que al estar luego del simbolo [$] PHP interpreta que son una variable.
+     "Y = $y <br>" . //La etiqueta <br> es de HTML que permite realizar un salto de linea.
+     "RESULTADO: $resultado";
+?>

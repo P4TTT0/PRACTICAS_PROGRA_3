522e18f49fba94e2db6f659d269e15929b2f2448
Patricio Perez Cardenal <patricio.perezcardenal@gmail.com>
2023-03-27 21:42:40 -0300

    PRACTICAS PROGRA_3 - Ejercicio 01

---
+++ clase01/ejercicios_Guia/Ejercicio_01.php
@@ -0,0 +1,12 @@
+<?php //TAG de apertura para indicar el inicio de codigo PHP.
+/*
+VARIABLES ESCALARES
+PHP Es un lenguaje de programacion de tipado dinámico
+El tipo de una variable es determinado automáticamente según el valor asignado
+*/
+$nombre = "Patricio"; // STRING
+$apellido = "Perez Cardenal"; // STRING
+
+echo $nombre . ", " . $apellido; // echo -> Palabra reservada para imprimir texto y/o variables a la salida de una pagina web.
+
+?>
